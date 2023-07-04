33bd940a52d0b146f087bfdf4e82af8f85878fba
javawithlwl <javawithlwl@gmail.com>
2023-07-04 08:49:08 +0530

    Add welcome message twice

--- src/main/java/com/careerit/cj/HelloWorld.java
+++ src/main/java/com/careerit/cj/HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         System.out.println("Welcome to Java World!");
+        System.out.println("Once again Welcome to Java World");
 
     }
 }
